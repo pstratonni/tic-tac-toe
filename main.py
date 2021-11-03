dce9ab9084c9d91717bc26faa566c0bca2734d98
Petr Stratonnikov <pstratonni@gmail.com>
2021-11-03 23:09:57 +0100

    final

--- main.py
+++ main.py
@@ -1,23 +1,4 @@
-winX = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0]
-]
-
-win0 = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0]
-]
-
-field = [
-    ['-', '-', '-'],
-    ['-', '-', '-'],
-    ['-', '-', '-']
-]
-
-
-def render_field():
+def render_field(field):
     for i in range(4):
         if i != 0:
             print(i, end=' ', flush=True)
@@ -31,35 +12,82 @@ def render_field():
         print('')
 
 
-def append_field(coor, who):
-    if any([i > 3 for i in coor]):
-        return True
-    if field[coor[0] - 1][coor[1] - 1] != '-':
-        return True
+def append_field(coor, who, field):
+    if all([0 < i < 4 for i in coor]):
+        if field[coor[0] - 1][coor[1] - 1] != '-':
+            return True
+        else:
+            field[coor[0] - 1][coor[1] - 1] = who
     else:
-        field[coor[0] - 1][coor[1] - 1] = who
+        return True
 
-
-def append_X_0(who):
+def append_X_0(who, field, win):
     while True:
         print(f"Введите координаты {'крестика' if who == 'X' else 'нолика'} через пробел (первая номер строки): ")
-        coor = tuple(map(int, input().split()))
-        if not append_field(coor, who):
-            append_field(coor, who)
-            render_field()
+        try:
+            coor = tuple(map(int, input().split()))
+        except ValueError:
+            print('Упс. Координаты - это числа, которые вводятся через пробел.\nПопробуй ещё раз')
+            continue
+        if not append_field(coor, who, field):
+            append_field(coor, who, field)
+            render_field(field)
+            count(coor, who, win)
             break
         else:
-            print("Поле занято, или неверные координаты Попробуй еще раз")
+            print("Поле занято, или неверные координаты. Попробуй еще раз")
 
 
-print("Начнем игру!!!")
-render_field()
-count = 0
-while True:
+def count(coor, who, win):
+    x, y = coor
+    key = 'win' + who
+    win[key][0][x - 1] += 1
+    win[key][1][y - 1] += 1
+    if x == y:
+        win[key][2][0] += 1
+    if x == y == 2 or abs(x - y) == 2:
+        win[key][2][1] += 1
 
-    append_X_0("X")
-    count += 1
-    if count == 9:
-        break
-    append_X_0("0")
-    count += 1
+
+def check_win(who, win):
+    lis = win['win' + who]
+    for line in lis:
+        if any([i == 3 for i in line]):
+            print(f"{'Крестик' if who == 'X' else 'Нолик'} победил")
+            return True
+    return False
+
+
+def play(field, win):
+    count_step = 0
+    while True:
+        append_X_0("X", field, win)
+        count_step += 1
+        if count_step == 9:
+            print("Ничья")
+            break
+        check = check_win("X", win)
+        if check:
+            break
+        append_X_0("0", field, win)
+        count_step += 1
+        check = check_win("0", win)
+        if check:
+            break
+
+
+def main():
+    while True:
+        field = [['-' for j in range(3)] for i in range(3)]
+        win = {'winX': [[0 for j in range(3)] for i in range(3)],
+               'win0': [[0 for j in range(3)] for i in range(3)]}
+        print("Начнем игру!!!")
+        render_field(field)
+        play(field, win)
+        ex = input('Хочешь сыграть ещё раз, жми y: ').lower()
+        if ex == 'y':
+            continue
+        exit(0)
+
+
+main()
